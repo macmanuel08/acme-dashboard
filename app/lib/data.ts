5c8df8a0c1e4578a761bc4e20566c3acba23e09a
John Mark Manuel <manueljm016@gmail.com>
2025-06-04 20:29:21 +0800

    streamning and partial prerendering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,15 +13,15 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
+    // Artificially delay a response for demo purposes for the Loading.tsx and <Suspense />.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -32,12 +32,21 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    // Artificially delay a response for demo purposes for the Loading.tsx and <Suspense />.
+    // Don't do this in production :)
+
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+     console.log('Data fetch completed after 3 seconds.'); 
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
@@ -52,15 +61,25 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+
+    // Artificially delay a response for demo purposes for the Loading.tsx and <Suspense />.
+    // Don't do this in production :)
+
+    console.log('Fetching cards data...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
+
+    console.log('Data fetch completed after 3 seconds.');
 
     const data = await Promise.all([
       invoiceCountPromise,
