52804590276f1b18402e17fe54b0df9f91c016b4
John Mark Manuel <manueljm016@gmail.com>
2025-06-27 15:51:19 +0800

    added adding invoice functionality

---
+++ app/lib/actions.ts
@@ -0,0 +1,38 @@
+'use server'; // marking all the exported functions within the file as Server Actions.
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100; // It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
+    const date = new Date().toISOString().split('T')[0]; // create a new date with the format "YYYY-MM-DD" for the invoice's creation date
+
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices'); // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server.
+    redirect('/dashboard/invoices'); // redirect the user back to the /dashboard/invoices page
+}
+
